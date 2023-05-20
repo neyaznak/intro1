f8a273a96522fba333428a20180bcaa086c2369e
MohammadNAK <109782209+neyaznak@users.noreply.github.com>
2023-05-20 14:04:32 +0530

    selection sort algorithm code java

---
+++ searching_sorting/SelectionSort.java
@@ -0,0 +1,30 @@
+package searching_sorting;
+import java.util.Scanner;
+public class SelectionSort {
+	
+	public static void main(String[] args) {
+		int [] arr = {2,6,7,1,0,8};
+		selectionSort(arr);
+		for(int i = 0; i < arr.length; i++) {
+		System.out.print(arr[i]+" ");
+		}
+	}
+
+	public static void selectionSort(int[] arr) {
+		for(int i = 0; i < arr.length-1; i++) {
+			int min = arr[i];
+			int minIndex = i;
+			for(int j = i + 1; j < arr.length; j++) {
+				if(arr[j] < min) {
+					min = arr[j];
+					minIndex =j;
+				}
+			}
+			if(minIndex != i) {
+				arr[minIndex] = arr[i];
+				arr[i] = min;
+			}
+		}
+	}
+
+}
