626a15f4b4e7811dde80819faf10dca5d4ea88b0
MohammadNAK <109782209+neyaznak@users.noreply.github.com>
2023-05-23 14:29:41 +0530

    recursion factorial

---
+++ recursion/Factorial.java
@@ -0,0 +1,22 @@
+package recursion;
+import java.util.Scanner;
+
+public class Factorial {
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int ans = fact(n);
+		System.out.println(ans);
+	}
+
+	public static int fact(int n) {
+		if(n == 1 || n == 0) {
+			return 1;
+		}
+		
+		int smallAns = fact(n-1);
+		return n * smallAns;
+	}
+	
+}
